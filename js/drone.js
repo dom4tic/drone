7b3dfb5289129e93b8221c831b21af4f5c9b38df
dom4tic <dominic.koch@gmx.ch>
2021-05-06 22:36:27 +0200

    better working sidebar

--- js/drone.js
+++ js/drone.js
@@ -2,7 +2,12 @@
     function clickNav() {
         if (document.getElementById("mySidebar").style.width == '250px') {
             document.getElementById("mySidebar").style.width = "0";
+            document.getElementById("sidebarButton").classList.remove("bi-x");
+            document.getElementById("sidebarButton").classList.add("bi-list");
+
         } else {
             document.getElementById("mySidebar").style.width = "250px";
+            document.getElementById("sidebarButton").classList.remove("bi-list");
+            document.getElementById("sidebarButton").classList.add("bi-x");
         }
     }
