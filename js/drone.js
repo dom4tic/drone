c4acc2cb2d6505dd4f0314f459177938cc6f2068
dom4tic <dominic.koch@gmx.ch>
2021-06-08 13:48:28 +0200

    offers collapse improved

--- js/drone.js
+++ js/drone.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#sidebarButton').click(function () {
+    $("#sidebarButton").click(function () {
         if (document.getElementById("mySidebar").style.width == '250px') {
             document.getElementById("mySidebar").style.width = "0";
             document.getElementById("sidebarButton").classList.remove("bi-x");
@@ -9,6 +9,18 @@ $(document).ready(function() {
             document.getElementById("mySidebar").style.width = "250px";
             document.getElementById("sidebarButton").classList.remove("bi-list");
             document.getElementById("sidebarButton").classList.add("bi-x");
+        }
+    });
+
+    $("#show").click(function () {
+        if (this.className.includes('plus')) {
+            $("#show").html('<img class="plus-button-icon" src="css/img/minus.png" alt="minus">show less');
+            $("#show").removeClass('plus');
+            $("#show").addClass('minus');
+        } else {
+            $("#show").html('<img class="plus-button-icon" src="css/img/plus.png" alt="plus">show more');
+            $("#show").removeClass('minus');
+            $("#show").addClass('plus');
         }
     });
 
